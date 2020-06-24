2fe2d3153f38d2d05a9c0190a2ef87e4a851125c
scottvonscott <skeplin17@gmail.com>
2020-06-24 12:15:37 -0700

    User signup and login views

--- config/environment.rb
+++ config/environment.rb
@@ -10,3 +10,8 @@ ActiveRecord::Base.establish_connection(
 
 require './app/controllers/application_controller'
 require_all 'app'
+
+
+use Rack::MethodOverride
+use UserController
+run ApplicationController
