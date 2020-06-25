82dc86fa81d28a92598f50a5165951ecb1c87c71
scottvonscott <skeplin17@gmail.com>
2020-06-24 22:01:25 -0700

    Continue on restaurant

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantController < ApplicationController
 
-    get '/restaurants'
+get '/restaurants' do
     verify_logged_in
     @restaurants = Restaurants.all
     erb :'restaurants/index'
@@ -8,11 +8,29 @@ end
 
 post '/restaurants' do
     verify_logged_in
+    if params[:name] == ""
+        # put error here
+        redirect to "/restaurants/new"
+    else
+        @restaurant = current_user.restaurants.build(name: params[:name], city: params[:city])
+        if @restaurant.save
+            redirect to "/restaurant/#{@restaurant.id}"
+        else
+            # put error here
+            redirect to "/restaurant/new"
+        end
+    end
 end
 
 get '/restaurants/new' do
     verify_logged_in
     erb :'restaurants/new'
+end
+
+get '/restaurants/:id' do
+    verify_logged_in
+        @restaurant = Restaurant.find_by_id(params[:id])
+        erb :'restaurants/show_restaurant'
 end
 
 
